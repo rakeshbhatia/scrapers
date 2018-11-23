4b329447b1211b49936c7c6c1d2353a3a8d3ed03
Rakesh <rakeshbhatia87@gmail.com>
2018-11-22 19:46:06 -0800

    trade analyzer v1

--- trade_analyzer.py
+++ trade_analyzer.py
@@ -1,3 +1,11 @@
+#Python Fantasy Sports Trade Analyzer (NFL/NBA) v1
+#Copyright 2018 Rakesh Bhatia
+
+#This web scraper utilizes Selenium and PhantomJS to
+#run the automated task of entering player names
+#into a trade analyzer website for analyzing fantasy
+#sports trades (www.fantasysp.com)
+
 import math
 import csv
 import time
@@ -24,14 +32,14 @@ def analyze_trade(league, left_players, right_players, headless):
     nfl_url = 'https://www.fantasysp.com/nfl_trade_analyzer/'
     nba_url = 'https://www.fantasysp.com/nba_trade_analyzer/'
 
+    # run without browser
     if headless:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--headless")
         chrome_options.binary_location = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
         driver = webdriver.Chrome(executable_path='/Users/rakeshbhatia/anaconda/envs/fantasy_mgr/chromedriver',   chrome_options=chrome_options)
 
-        #DesiredCapabilities.PHANTOMJS['phantomjs.page.settings.userAgent'] = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:16.0) Gecko/20121026 Firefox/16.0'
-        #driver = webdriver.PhantomJS(executable_path='/Users/rakeshbhatia/anaconda/envs/fantasy_mgr/bin/phantomjs')
+    # otherwise, set browser parameters
     else:
         chrome_options = webdriver.ChromeOptions()
         driver = webdriver.Chrome(executable_path='/Users/rakeshbhatia/anaconda/envs/fantasy_mgr/chromedriver', chrome_options=chrome_options)
@@ -45,29 +53,32 @@ def analyze_trade(league, left_players, right_players, headless):
         elif league == 'nba':
             driver.get(nba_url)
 
+        # type in player names for left side of trade
         print('Entering left players')
         for i in range(len(left_players)):
             driver.find_element_by_name('leftplayer' + str(i+1)).send_keys(left_players[i])
 
+        # type in player names for right side of trade
         print('Entering right players')
         for i in range(len(right_players)):
             driver.find_element_by_name('rightplayer' + str(i+1)).send_keys(right_players[i])
 
+        # wait a random period before submitting
         wait_time = round(max(2, 6 + random.gauss(0,3)), 2)
         time.sleep(wait_time)
 
+        # click submit button
         print('Clicking submit')
         driver.find_element_by_class_name('analyze-trade-submit').click()
 
         wait_time = round(max(2, 6 + random.gauss(0,3)), 2)
         time.sleep(wait_time)
 
-        #left_div = driver.find_element_by_class_name('point-total')
+        # extract data from results page
         left_span = driver.find_element_by_class_name('icon-left')
         left_result = left_span.get_attribute('tally')
         print('left_players: ', left_result)
 
-        #right_div = driver.find_element_by_class_name('point-total')
         right_span = driver.find_element_by_class_name('icon-right')
         right_result = right_span.get_attribute('tally')
         print('right_players: ', right_result)
@@ -87,26 +98,13 @@ def analyze_trade(league, left_players, right_players, headless):
 
 def main():
     start_time = time.time()
-    #left_players = ['']
-    #right_players = ['']
-    #left_players = ['']
-    #right_players = ['']
-    #left_players = ['']
-    #right_players = ['']
-    #left_players = ['']
-    #right_players = ['']
-    #left_players = ['']
-    #right_players = ['']
-    #left_players = ['Chris Paul', 'Andre Drummond', 'Markieff Morris', 'Jordan Bell']
-    #right_players = ['Robert Covington', 'Clint Capela', 'Jarrett Allen', 'Kris Dunn']
+
     left_players = ['Ben Simmons', 'DeAndre Jordan', 'Carmelo Anthony', 'Dirk Nowitzki']
     right_players = ['Joe Ingles', 'Rudy Gobert', 'Dejounte Murray', 'Kristaps Porzingis']
-    #left_players = ['Antonio Brown', 'Christian McCaffrey']
-    #right_players = ['DeAndre Hopkins', 'Kareem Hunt']
-    #left_players = ['Le\'Veon Bell', 'Jay Ajayi']
-    #right_players = ['Ezekiel Elliott', 'Derrick Henry']
-    headless = False
+
+    headless = True
     league = 'nba'
+
     analyze_trade(league, left_players, right_players, headless)
     print("--- %s seconds ---" % (time.time() - start_time))
 
